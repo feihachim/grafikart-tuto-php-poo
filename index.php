640e62d3e71e947f08eef729afdd6ea53410657f
Hachim Youssoufa <feihachim@yahoo.fr>
2021-12-23 18:53:07 +0100

    re

--- index.php
+++ index.php
@@ -9,13 +9,6 @@ $harry = new Personnage("Harry");
 echo $merlin->getNom();
 
 $merlin->attaque($harry);
-if ($harry->vie <= 0)
-{
-    echo "Harry est mort :(";
-}
-else
-{
-    echo "Harry a survécu avec $harry->vie points de vie<br>";
-}
+
 var_dump($merlin);
 var_dump($harry);
